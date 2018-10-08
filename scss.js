3fa96349d94a2740310f9c84ec9c7ca2138510fd
Lin Jinxing <172601673@qq.com>
2018-10-08 22:21:44 +0800

    规范化jade

---
+++ scss.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const fs = require('fs');
+const sass = require('sass');
+
+function debounce(fn, wait) {
+    let timer = 0;
+    return () => {
+        if (!!timer) clearTimeout(timer);
+        timer = setTimeout(() => fn.apply(this, arguments), wait);
+    };
+}
+
+const input = path.join(__dirname, 'src', 'scss', 'apollo.scss');
+const output = path.join(__dirname, 'source', 'css', 'apollo.css');
+
+function build() {
+    sass.render({ file: input, outputStyle: 'compressed' }, (error, result) => {
+        if (error) throw error;
+        fs.writeFile(output, result.css, (error) => {
+            if (error) throw error;
+        })
+    })
+    console.log('build done');
+}
+
+function watch() {
+    console.log('start watch');
+    const debounceBuild = debounce(build, 5000);
+    fs.watch('./src', { recursive: true }, (event, file) => {
+        debounceBuild();
+    })
+}
+
+const operate = {
+    '--build': build,
+    '--watch': watch
+}
+for (const key of process.argv) {
+    if (key in operate) operate[key].call();
+}
