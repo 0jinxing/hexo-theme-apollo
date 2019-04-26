a8c9f7f111b9b62bb30ab4b63c5ac20e8b8218bb
Jinxing Lin <172601673@qq.com>
2019-04-26 11:17:25 +0800

    timeout

--- scss.js
+++ scss.js
@@ -1,41 +1,41 @@
-const path = require('path');
-const fs = require('fs');
-const sass = require('sass');
+const path = require("path");
+const fs = require("fs");
+const sass = require("sass");
 
 function debounce(fn, wait) {
-    let timer = 0;
-    return () => {
-        if (!!timer) clearTimeout(timer);
-        timer = setTimeout(() => fn.apply(this, arguments), wait);
-    };
+  let timer = 0;
+  return () => {
+    if (!!timer) clearTimeout(timer);
+    timer = setTimeout(() => fn.apply(this, arguments), wait);
+  };
 }
 
-const input = path.join(__dirname, 'src', 'scss', 'apollo.scss');
-const output = path.join(__dirname, 'source', 'style', 'apollo.css');
+const input = path.join(__dirname, "src", "scss", "apollo.scss");
+const output = path.join(__dirname, "source", "style", "apollo.css");
 
 function build() {
-    sass.render({ file: input, outputStyle: 'compressed' }, (error, result) => {
-        if (error) throw error;
-        fs.writeFile(output, result.css, (error) => {
-            if (error) throw error;
-        })
-    })
-    console.log('build done');
+  sass.render({ file: input, outputStyle: "compressed" }, (error, result) => {
+    if (error) throw error;
+    fs.writeFile(output, result.css, error => {
+      if (error) throw error;
+    });
+  });
+  console.log("build done");
 }
 
 function watch() {
-    console.log('start watch');
-    const debounceBuild = debounce(build, 5000);
-    fs.watch('./src', { recursive: true }, (event, file) => {
-        debounceBuild();
-    })
+  console.log("start watch");
+  const debounceBuild = debounce(build, 5000);
+  fs.watch("./src", { recursive: true }, (event, file) => {
+    debounceBuild();
+  });
 }
 
 const operate = {
-    '--build': build,
-    '--watch': watch
-}
+  "--build": build,
+  "--watch": watch
+};
 
 for (const key of process.argv) {
-    if (key in operate) operate[key].call();
+  if (key in operate) operate[key].call();
 }
